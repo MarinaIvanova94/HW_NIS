32f6ec213527d312e7fe6a4ebc7a68009ebf4413
svetaskuchilina <sveta26_94@mail.ru>
2015-11-03 19:11:11 +0300

    Update Skuch_ info from site.py

--- Skuchilina_Svetlana/Skuch_ info from site.py
+++ Skuchilina_Svetlana/Skuch_ info from site.py
@@ -1,18 +1,18 @@
 # -*- coding: utf-8 -*-
-
 import lxml.html as html
 tag=[]
 url_list=[]
-article_list=[]
-root = html.parse("http://rusrep.ru/authors/199670/").getroot()
-tag.extend(root.find_class("left"))
+title_list=[]
+for i in range(1,493):
+    root = html.parse('http://kinogo.co/page/{0}/'.format(i)).getroot()
+    tag.extend(root.find_class('shortstory'))
 for i in tag:
     for j in i.iterlinks():
-        if j[2] == "/search_tags/176309/":
-            for y in i.find_class('title').pop().iterlinks():
-                if 'article' in y[2]:
-                    url_list.append('http://rusrep.ru' + y[2])
-            article_list.append(i.find_class('sub_title').pop().text_content().strip())
+        if 'indijskie_filmy' in j[2]:
+            for y in i.find_class('zagolovki').pop().iterlinks():
+                if '2010' in y[2]:
+                    url_list.append(y[2])
+                    title_list.append(i.find_class('zagolovki').pop().text_content())
 for i in range(len(url_list)):
     print url_list[i]
-    print article_list[i]
+    print title_list[i]
