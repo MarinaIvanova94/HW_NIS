7ae5deef2410690c1da80523e248b0a8d3e9e8e1
ZaplatkinaIrina <era555@list.ru>
2015-11-04 17:08:13 +0300

    Домашняя работа №3

---
+++ Zaplatkina_Irina/Zaplatkina_3.py
@@ -0,0 +1,11 @@
+import lxml.html as html
+
+page = html.parse('http://www.planetaexcel.ru/video/')
+root = page.getroot()
+tag = root.find_class('s2u-yt-grid-collection').pop()
+url_list = []
+
+for i in tag.iterlinks():
+        if i[2].find('http://www.youtube.com/') != -1:
+            url_list.append(i[2])
+print('\n'.join(url_list))
