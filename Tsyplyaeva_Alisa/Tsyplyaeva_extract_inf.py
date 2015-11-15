72b89f559a03b0b0e041df49883cd6f06c2e1123
tsyplyaeva-alisa <tsyplyaeva-alisa@users.noreply.github.com>
2015-11-15 14:40:23 +0300

    Create Tsyplyaeva_extract_inf.py

---
+++ Tsyplyaeva_Alisa/Tsyplyaeva_extract_inf.py
@@ -0,0 +1,30 @@
+# extracting links and plot summaries of Disney movies
+
+import lxml.html as html
+
+page = html.parse('http://www.movieinsider.com/c2/walt-disney-pictures/')
+root = page.getroot()
+tag = root.find_class("phase5")
+
+url_list = []
+
+for i in tag:    
+    for j in i.iterlinks():
+        if ('movies' not in j[2]) and ('page_offset' not in j[2]):
+            url_list.append(j[2])
+print(url_list)
+
+
+annonce_list = []
+
+for url in url_list:
+    page1 = html.parse(url)
+    root1 = page1.getroot()
+    tag1 = root1.find_class('plot')#.pop()
+    
+    for i in tag1:
+        an = i.text_content().strip()
+        annonce_list.append(an)
+        
+for index, url in url_list:
+    print (url, annonce_list[index])
