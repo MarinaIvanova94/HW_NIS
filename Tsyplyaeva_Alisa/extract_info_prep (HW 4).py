f99592bbc8834faa86400c6b6f34f597d366c96f
tsyplyaeva-alisa <alyson_95@mail.ru>
2015-11-16 22:28:47 +0300

    HW 4

---
+++ Tsyplyaeva_Alisa/extract_info_prep (HW 4).py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 15 00:10:39 2015
+
+@author: Alisa
+"""
+
+# extracting links and plot summaries of Disney movies
+
+import lxml.html as html
+import pickle
+
+page = html.parse('http://www.movieinsider.com/c2/walt-disney-pictures/')
+root = page.getroot()
+tag = root.find_class("phase5")
+
+url_list = []
+
+f = open('output_links.txt', 'w')
+for i in tag:    
+    for j in i.iterlinks():
+        if (('movies' not in j[2]) and ('page_offset' not in j[2]) 
+        and ('status' not in j[2]) and ('videos' not in j[2])):
+            url_list.append(j[2])
+            f.write(j[2] + '\n')
+f.close()
+
+
+annonce_list = []
+title_list = []
+year_list = []
+duration_list = []
+
+for url in url_list:
+    page1 = html.parse(url)
+    root1 = page1.getroot()
+    tag1 = root1.find_class('plot').pop()
+    tag2 = root1.xpath('/html/body/div[2]/div/h3').pop()    
+    tag3 = root1.xpath('/html/body/div[3]/div/div/div[1]/div[2]/div/div[2]/span[1]').pop()
+
+
+    annonce_list.append(tag1.text_content()) # + '\n')
+    title_list.append(tag2.text_content()) # + '\n')
+    year_list.append(tag3.text_content()) # + '\n')
+
+
+f = open('output_Disney.txt', 'w')    
+
+for i in range(len(url_list)):
+    f.write(url_list[i] + '\t')
+    f.write(title_list[i] + '\t')
+    f.write(year_list[i]  + '\t')
+    try:
+        f.write(annonce_list[i].decode() + '\n')
+    except:
+        f.write('\n')
+        continue
+f.close()
+
+    
+# print(len(annonce_list))
+# print(len(title_list))
+# print(len(year_list))
+
+
