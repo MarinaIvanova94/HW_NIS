8057f029d525861e6f75e3adbf802a417c505599
PolinaBlin <blinova2510@gmail.com>
2015-12-13 16:50:55 +0300

    Update Laba2.py

--- Bardina Alyona/Laba2.py
+++ Bardina Alyona/Laba2.py
@@ -1,6 +1,6 @@
 # -*- coding: UTF-8 -*-
 from lxml import etree, html
-f = open('E:\HW_NIS\Blinova Polina\Blinova.xml')
+f = open('D:\HW_NIS\Blinova Polina\Blinova.xml')
 tree = etree.parse(f)
 root = tree.getroot()
 for element in root.iter('author'):
@@ -8,7 +8,7 @@ for element in root.iter('author'):
     etree.SubElement(element,'first_name', name = " ".join(mylist[:-1]))
     etree.SubElement(element,'last_name', name = " ".join(mylist[-1]))
 output = etree.tostring(root, pretty_print=True, encoding='UTF-8')
-f1 = open('E:\HW_NIS\Bardina Alyona\Bardina(Blinova).xml', 'w')
+f1 = open('D:\HW_NIS\Bardina Alyona\Bardina(Blinova).xml', 'w')
 f1.write(output)
 f1.close()
 f.close()
