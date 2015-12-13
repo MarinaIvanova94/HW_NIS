08e1c1427b59bb18b75e8fde494ecdbef63f0d41
PolinaBlin <blinova2510@gmail.com>
2015-12-13 18:47:22 +0300

    Create 4 laba (site_xml).py

---
+++ Bardina Alyona/4 laba (site_xml).py
@@ -0,0 +1,52 @@
+# -*- coding: UTF-8 -*-
+import lxml.html as html
+from lxml.etree import Element, SubElement, ElementTree
+from lxml import etree
+tag=[]
+url_list=[]
+title_list=[]
+country_list=[]
+time_list = []
+actor_list = []
+for i in range(1,225):
+    root = html.parse('http://gidonline.club/genre/komediya/page/{0}/'.format(i)).getroot()
+    tag.extend(root.find_class('mainlink'))
+for i in tag:
+    try:
+        y = i.find_class('mqn').pop().text_content()
+        if y == '2013':
+            for y in i.find_class('mainlink').pop().iterlinks():
+                if y[1] == 'href':
+                    url_list.append(y[2])
+            title_list.append(i.text_content().split('\r')[0])
+    except IndexError:
+        y = i.find_class('mqx').pop().text_content()
+
+for ind, url in enumerate(url_list):
+    page1 = html.parse(url)
+    root1 = page1.getroot()
+    tag1 = root1.find_class('t-row').pop().text_content().strip().split('\n')
+    country_list.append(tag1[2].strip()[len(u'страна'):])
+    time_list.append(tag1[4].strip()[len(u'время'):])
+
+import pickle
+f=open('output.pickle', 'wb')
+pickle.dump('asd', f)
+f.close()
+f=open('output.pickle', 'rb')
+top_words=pickle.load(f)
+f.close()
+
+root = Element('list_of_comedies')
+for i in range(0, len(url_list)):
+    comedy = SubElement(root, 'comedy')
+    SubElement(comedy, 'title', href = url_list[i]).text = title_list[i]
+    SubElement(comedy, 'country').text = country_list[i]
+    time = SubElement(comedy, 'time')
+    time.text = time_list[i]
+    min = SubElement(time, 'minutes', number = str(int(time_list[i].split()[0]) * 60+ int(time_list[i].split()[2])))
+output = etree.tostring(root, pretty_print=True, encoding='UTF-8')
+f1 = open(r'E:\HW_NIS\Bardina Alyona\4 laba (site_xml).xml', 'wb')
+f1.write(output)
+print(output)
+f1.close()
