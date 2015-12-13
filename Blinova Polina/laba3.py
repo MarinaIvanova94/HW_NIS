f1bfd657925434612219ba0d3b74fe252bea9a40
PolinaBlin <blinova2510@gmail.com>
2015-12-13 17:43:03 +0300

    Update laba3.py

--- Blinova Polina/laba3.py
+++ Blinova Polina/laba3.py
@@ -1,7 +1,7 @@
 import lxml.html as html
 tag=[]
 url_list=[]
-inf_list=[]
+title_list=[]
 for i in range(1,137):
     root = html.parse('http://gidonline.club/genre/melodrama/page/{0}/'.format(i)).getroot()
     tag.extend(root.find_class('mainlink'))
@@ -12,8 +12,9 @@ for i in tag:
             for y in i.find_class('mainlink').pop().iterlinks():
                 if y[1] == 'href':
                     url_list.append(y[2])
-            inf_list.append(i.text_content())
+            title_list.append(i.text_content().split('\n')[0])
     except IndexError:
         y = i.find_class('mqx').pop().text_content()
-for i in range(len(inf_list)):
-    print url_list[i], inf_list[i], '\n'
+for i in range(len(url_list)):
+    print "Title: ", title_list[i]
+    print "URL: ", url_list[i], '\n'
